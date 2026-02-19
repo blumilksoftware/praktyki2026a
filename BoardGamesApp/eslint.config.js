380a4afb279711215f8d43eebbb440bf78caede2
Strzelacz48 <strzelacz@Strzelacz48-NPC>
2026-02-19 13:48:40 +0100

    chore: removed duplicate catalogue

--- BoardGamesApp/eslint.config.js
+++
@@ -1,5 +0,0 @@
-import blumilkDefault from '@blumilksoftware/eslint-config'
-
-export default [
-  ...blumilkDefault,
-]
--- BoardGamesApp/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Illuminate\Support\Facades\Route;
-use Inertia\Response;
-
-Route::get("/", fn(): Response => inertia("Welcome"));
