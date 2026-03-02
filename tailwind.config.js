8866c722135e40008c701d6dc23d88b81a9d326f
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2026-03-02 12:27:04 +0100

    #2 - trying to fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 import forms from '@tailwindcss/forms'
 
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.vue',
-  ],
-
   theme: {
     extend: {
       fontFamily: {
@@ -17,6 +9,5 @@ export default {
       },
     },
   },
-
   plugins: [forms],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,53 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    vue(),
-  ],
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-  server: {
-    hmr: {
-      host: 'boardgameapp.blumilk.local.env',
-      protocol: 'wss',
+  return defineConfig({
+    build: {
+      outDir: './public/build/',
     },
-
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,        
-  },
-})
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: true,
+      origin: 'https://' + process.env.VITE_DEV_SERVER_DOCKER_HOST_NAME,
+      cors: true, // Allow any origin
+      watch: {
+        ignored: [
+          '**/.idea/**',
+          '**/app/**',
+          '**/tests/**',
+          '**/bootstrap/**',
+          '**/public/**',
+          '**/vendor/**',
+          '**/storage/**',
+          '**/node_modules/**',
+        ],
+      },
+    },
+    resolve: {
+      alias: {
+        '@': '/resources/js',
+      },
+    },
+    plugins: [
+      laravel({
+        input: 'resources/js/app.ts',
+        refresh: true,
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
+        },
+      }),
+      tailwindcss(),
+    ],
+  })
+}
